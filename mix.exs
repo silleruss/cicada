32956945fe13eb9f7da39bf0c0aec50e1d6b5ba7
srlee <silleruss@gmail.com>
2022-06-18 22:35:33 +0900

    feat: mongodb implement

--- mix.exs
+++ mix.exs
@@ -36,7 +36,6 @@ defmodule Cicada.MixProject do
       {:phoenix, "~> 1.6.6"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
-      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
@@ -48,7 +47,8 @@ defmodule Cicada.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:mongodb, ">= 0.0.0"}
     ]
   end
 
